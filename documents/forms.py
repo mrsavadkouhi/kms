5efdefca48af97adcf7425627e9dff5da0638b80
mrsavadkouhi <morteza.rostami.savadkouhi@gmail.com>
2022-08-04 15:26:56 +0430

    export excel, import articles

--- documents/forms.py
+++ documents/forms.py
@@ -220,23 +220,10 @@ class CenterAttachmentForm(BSModalModelForm):
 
 class DocumentImportForm(forms.Form):
     excel_file = forms.FileField()
+    doc_type = forms.CharField()
 
-    def clean(self):
-        cleaned_data = super().clean()
-
-        excel_file = cleaned_data['excel_file']
-
-        wb = openpyxl.load_workbook(excel_file)
-        worksheet = wb["Sheet1"]
-
-        excel_data = list()
-        for row in worksheet.iter_rows():
-            row_data = list()
-            for cell in row:
-                row_data.append(str(cell.value))
-            excel_data.append(row_data)
-
-        for row in excel_data[1:]:
+    def import_articles(self, excel_data):
+        for row in excel_data:
             manager_name = row[2]
             manager_name = manager_name.split(' ')
             manger_first_name = manager_name[0]
@@ -251,19 +238,19 @@ class DocumentImportForm(forms.Form):
             if started_at == 'None':
                 started_at = ''
             else:
-                started_at = datetime.datetime.strptime(started_at, "%Y-%m-%d %H:%M:%S")
+                started_at = datetime.strptime(started_at, "%Y-%m-%d %H:%M:%S")
 
             to_be_finished = row[5]
             if to_be_finished == 'None':
                 to_be_finished = ''
             else:
-                to_be_finished = datetime.datetime.strptime(to_be_finished, "%Y-%m-%d %H:%M:%S")
+                to_be_finished = datetime.strptime(to_be_finished, "%Y-%m-%d %H:%M:%S")
 
             finished_at = row[8]
             if finished_at == 'None':
                 finished_at = ''
             else:
-                finished_at = datetime.datetime.strptime(finished_at, "%Y-%m-%d %H:%M:%S")
+                finished_at = datetime.strptime(finished_at, "%Y-%m-%d %H:%M:%S")
 
             payment = row[6]
             if payment:
@@ -285,5 +272,68 @@ class DocumentImportForm(forms.Form):
                 Project.objects.create(name=row[0], description=row[1], manager=manager, center=center, started_at=started_at, finished_at=finished_at, to_be_finished=to_be_finished, payment=payment, paid=paid, status=status)
             except:
                 pass
+
+    def clean(self):
+        cleaned_data = super().clean()
+        doc_type = cleaned_data['doc_type']
+
+        excel_file = cleaned_data['excel_file']
+
+        wb = openpyxl.load_workbook(excel_file)
+        worksheet = wb["Sheet1"]
+
+        excel_data = list()
+        for row in worksheet.iter_rows():
+            row_data = list()
+            for cell in row:
+                row_data.append(str(cell.value))
+            excel_data.append(row_data)
+
+        if doc_type == 'Article':
+            self.import_articles(excel_data[1:])
+        elif doc_type == 'Book':
+            pass
+        elif doc_type == 'Experience':
+            pass
+        elif doc_type == 'Idea':
+            pass
+        elif doc_type == 'Thesis':
+            pass
+        elif doc_type == 'Manual':
+            pass
+        elif doc_type == 'Order':
+            pass
+        elif doc_type == 'Seminar':
+            pass
+        elif doc_type == 'Project':
+            pass
+        elif doc_type == 'Conference':
+            pass
+        elif doc_type == 'Visit':
+            pass
+        elif doc_type == 'Report':
+            pass
+        elif doc_type == 'Resume':
+            pass
+        elif doc_type == 'Center':
+            pass
+        elif doc_type == 'Core':
+            pass
+        elif doc_type == 'Tech':
+            pass
+        elif doc_type == 'Company':
+            pass
+        elif doc_type == 'Future':
+            pass
+        elif doc_type == 'Journal':
+            pass
+        elif doc_type == 'Cowork':
+            pass
+        elif doc_type == 'Invention':
+            pass
+        elif doc_type == 'Assessment':
+            pass
+        elif doc_type == 'Workshop':
+            pass
 
         return cleaned_data
