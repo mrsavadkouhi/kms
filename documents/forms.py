8ad79275904c316a11f3bb015ce091ca76441ce8
mrsavadkouhi <morteza.rostami.savadkouhi@gmail.com>
2022-08-09 21:41:23 +0430

    utf8 with bom

--- documents/forms.py
+++ documents/forms.py
@@ -1116,6 +1116,7 @@ class DocumentImportForm(forms.Form):
             raise forms.ValidationError("هیچ فایلی انتخاب نشده است.")
 
         df = pd.read_csv(excel_file)
+        # df = pd.read_excel(excel_file)
 
         try:
             if doc_type == 'Article':
